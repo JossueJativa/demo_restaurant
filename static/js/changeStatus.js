0c976f63e64428275fd6d2e8a081038bcb6ff235
Jossue <jativatamayojossuedavid@gmail.com>
2024-01-04 10:46:04 -0500

    Omitir codigo repetido

--- static/js/changeStatus.js
+++ static/js/changeStatus.js
@@ -1,6 +1,7 @@
-function changeStatusPreparando(id) {
-    var url = `http://127.0.0.1:8080/api/orders/${id}/`;
-    var data = { "status": "Preparando" };
+function changeOrderStatus(id, newStatus) {
+    const url = `http://127.0.0.1:8080/api/orders/${id}/`;
+    const data = { "status": newStatus };
+
     fetch(url, {
         method: 'PATCH',
         body: JSON.stringify(data),
@@ -8,37 +9,19 @@ function changeStatusPreparando(id) {
             'Content-Type': 'application/json; charset=UTF-8'
         },
     })
-        .then(response => response.json())
-        .then(json => console.log(json))
-    location.reload();
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .finally(() => location.reload());
+}
+
+function changeStatusPreparando(id) {
+    changeOrderStatus(id, "Preparando");
 }
 
 function changeStatusListo(id) {
-    var url = `http://127.0.0.1:8080/api/orders/${id}/`;
-    var data = { "status": "Listo" };
-    fetch(url, {
-        method: 'PATCH',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        },
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
-    location.reload();
+    changeOrderStatus(id, "Listo");
 }
 
 function changeStatusEntregado(id) {
-    var url = `http://127.0.0.1:8080/api/orders/${id}/`;
-    var data = { "status": "Entregado" };
-    fetch(url, {
-        method: 'PATCH',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        },
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
-    location.reload();
+    changeOrderStatus(id, "Entregado");
 }
